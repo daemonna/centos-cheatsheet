e86b19f1c61f6a206cd30207a0a16a90b8ba1dca
peter ducai <daemonna@users.noreply.github.com>
2016-03-29 15:36:53 +0200

    Update kvm.sh

--- scripts/kvm.sh
+++ scripts/kvm.sh
@@ -2,6 +2,7 @@
 
 grep -e 'vmx' /proc/cpuinfo
 
+qemu-img create -f qcow2 disk.img 5G
 
 virt-install --name Cent1 --ram 512 --disk path=/tmp/winxp.img,size=6 --network network:default --vnc --os-variant centos --cdrom /dev/sr0
 
